e00917890675f0c9c5e4b3b085532177bf30dc6f
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-11-08 21:34:48 +0200

    change standart shablon

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
-import "modern-normalize";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { BrowserRouter } from "react-router-dom";
-import { persistor, store } from "./redux/store";
-import { App } from "./components/App/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import './index.css';
 
-import AOS from "aos";
-
-// window.global = { BASE_URL: 'http://localhost:3030/api' };
-window.global = { BASE_URL: "https://rich-rose-shoulder-pads.cyclic.app/api" };
-AOS.init();
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={"Loading"} persistor={persistor}>
-        <BrowserRouter basename="finance-club">
-          {/* <BrowserRouter basename="/"> */}
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
