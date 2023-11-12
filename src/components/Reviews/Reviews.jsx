ed6979f909116b4ca238a1999f1ccc8dc5a62685
Heorgii <103379960+Heorgii@users.noreply.github.com>
2023-11-12 15:09:39 +0200

    start

---
+++ src/components/Reviews/Reviews.jsx
@@ -0,0 +1,21 @@
+import {
+  Container,
+  Section,
+  Title,
+} from 'components/baseStyles/CommonStyle.styled';
+
+export const Reviews = () => {
+  return (
+    <Section>
+      <Container>
+        <Title>Reviews</Title>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </Container>
+    </Section>
+  );
+};
