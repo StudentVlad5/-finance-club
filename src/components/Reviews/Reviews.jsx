da16cbcf9a5dd062d0e35c72502e9b13a60fcf2c
Heorgii <103379960+Heorgii@users.noreply.github.com>
2023-11-12 15:40:35 +0200

    s

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -3,18 +3,58 @@ import {
   Section,
   Title,
 } from 'components/baseStyles/CommonStyle.styled';
+import man from 'images/reviews/man.webp';
+import man2 from 'images/reviews/man2.webp';
+import w1 from 'images/reviews/w1.webp';
+import w2 from 'images/reviews/w2.webp';
+import {
+  ReviewsList,
+  ReviewsListItem,
+  ReviewsListItemDiscr,
+  ReviewsListItemName,
+} from './Reviews.styled';
 
 export const Reviews = () => {
+  const reviews = [
+    {
+      img: w1,
+      name: 'Jennifer Thompson',
+      discr:
+        "Being a part of the FinanceElite closed club has truly been a game-changer for me. The insights and strategies shared within the community are invaluable. It's more than just a club; it's a network of professionals navigating the financial landscape successfully.",
+    },
+    {
+      img: man,
+      name: 'Brian Mitchell',
+      discr:
+        "FinanceElite has exceeded my expectations. Joining the closed club has given me access to unique analytics and expert knowledge. I now have a significant advantage in the financial markets, and I couldn't be more grateful.",
+    },
+    {
+      img: w2,
+      name: 'Alexandra Turner',
+      discr:
+        "Unveiling the secrets of the financial world with FinanceElite! The exceptional recommendations and behind-the-scenes insights from industry professionals make this club outstanding. It's the perfect community for those who value quality and confidentiality.",
+    },
+    {
+      img: man2,
+      name: 'Ethan Reynolds',
+      discr:
+        "FinanceElite is more than a club; it's a community where you can learn, discuss, and make successful investments. Thanks for providing a unique perspective on the world of finance and delivering a highly qualified approach!",
+    },
+  ];
+
   return (
     <Section>
       <Container>
         <Title>Reviews</Title>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
+        <ReviewsList>
+          {reviews.map((review, idx) => (
+            <ReviewsListItem key={idx}>
+              <img width="70" height="70" src={review.img} alt="" />
+              <ReviewsListItemName>{review.name}</ReviewsListItemName>
+              <ReviewsListItemDiscr>{review.discr}</ReviewsListItemDiscr>
+            </ReviewsListItem>
+          ))}
+        </ReviewsList>
       </Container>
     </Section>
   );
---
+++ src/components/Reviews/Reviews.styled.js
@@ -0,0 +1,30 @@
+import { theme } from 'components/baseStyles/Variables.styled';
+import styled from 'styled-components';
+
+export const ReviewsList = styled.ul``;
+
+export const ReviewsListItem = styled.li`
+  width: 320px;
+  height: 162px;
+  border-radius: 40px;
+  background: #fff;
+`;
+
+export const ReviewsListItemName = styled.p`
+  color: ${theme.light};
+  font-family: ${theme.fonts[0]};
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 19.504px; 
+`;
+
+export const ReviewsListItemDiscr = styled.p``;
+
+// export const ReviewsList = styled.ul``
+// export const ReviewsList = styled.ul``
+// export const ReviewsList = styled.ul``
+// export const ReviewsList = styled.ul``
+// export const ReviewsList = styled.ul``
+// export const ReviewsList = styled.ul``
+// export const ReviewsList = styled.ul``
