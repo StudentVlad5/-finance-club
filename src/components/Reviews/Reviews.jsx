b7c6b4db0d15d5e850b35b7e3ab7fa3bed1b1af1
Heorgii <103379960+Heorgii@users.noreply.github.com>
2023-11-13 00:01:24 +0200

    reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -8,53 +8,81 @@ import man2 from 'images/reviews/man2.webp';
 import w1 from 'images/reviews/w1.webp';
 import w2 from 'images/reviews/w2.webp';
 import {
+  ReviewsBtn,
+  ReviewsBtnBox,
   ReviewsList,
   ReviewsListItem,
   ReviewsListItemDiscr,
+  ReviewsListItemImg,
   ReviewsListItemName,
 } from './Reviews.styled';
+import { useState } from 'react';
 
 export const Reviews = () => {
+  const initialReviews = 4;
+  const [reviewsNumber, setReviewsNumber] = useState(initialReviews);
+
   const reviews = [
     {
       img: w1,
       name: 'Jennifer Thompson',
       discr:
-        "Being a part of the FinanceElite closed club has truly been a game-changer for me. The insights and strategies shared within the community are invaluable. It's more than just a club; it's a network of professionals navigating the financial landscape successfully.",
+        'Being a part of the FinanceElite closed club has truly been a game-changer for me.',
     },
     {
       img: man,
       name: 'Brian Mitchell',
       discr:
-        "FinanceElite has exceeded my expectations. Joining the closed club has given me access to unique analytics and expert knowledge. I now have a significant advantage in the financial markets, and I couldn't be more grateful.",
+        "I now have a significant advantage in the financial markets, and I couldn't be more grateful.",
     },
     {
       img: w2,
       name: 'Alexandra Turner',
       discr:
-        "Unveiling the secrets of the financial world with FinanceElite! The exceptional recommendations and behind-the-scenes insights from industry professionals make this club outstanding. It's the perfect community for those who value quality and confidentiality.",
+        "It's the perfect community for those who value quality and confidentiality.",
     },
     {
       img: man2,
       name: 'Ethan Reynolds',
       discr:
-        "FinanceElite is more than a club; it's a community where you can learn, discuss, and make successful investments. Thanks for providing a unique perspective on the world of finance and delivering a highly qualified approach!",
+        'Thanks for providing a unique perspective on the world of finance and delivering a highly qualified approach!',
+    },
+    {
+      img: w2,
+      name: 'Alexandra Turner',
+      discr:
+        "It's the perfect community for those who value quality and confidentiality.",
+    },
+    {
+      img: man2,
+      name: 'Ethan Reynolds',
+      discr:
+        'Thanks for providing a unique perspective on the world of finance and delivering a highly qualified approach!',
     },
   ];
 
+  const handleReviewsNumber = () => {
+    setReviewsNumber(reviewsNumber + 4);
+  };
   return (
     <Section>
       <Container>
         <Title>Reviews</Title>
         <ReviewsList>
-          {reviews.map((review, idx) => (
+          {reviews.slice(0, reviewsNumber).map((review, idx) => (
             <ReviewsListItem key={idx}>
-              <img width="70" height="70" src={review.img} alt="" />
+              <ReviewsListItemImg src={review.img} alt="" />
               <ReviewsListItemName>{review.name}</ReviewsListItemName>
               <ReviewsListItemDiscr>{review.discr}</ReviewsListItemDiscr>
             </ReviewsListItem>
           ))}
         </ReviewsList>
+        <ReviewsBtnBox>
+        <ReviewsBtn onClick={handleReviewsNumber}>
+          <span> VIEW MORE </span>
+        </ReviewsBtn>
+        </ReviewsBtnBox>
+     
       </Container>
     </Section>
   );
