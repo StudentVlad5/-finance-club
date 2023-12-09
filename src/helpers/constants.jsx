8ab07d836e0eb4a040ec91079be8decb4f74b08a
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-12-09 20:25:38 +0200

    BASE_URL fix

--- src/helpers/constants.jsx
+++ src/helpers/constants.jsx
@@ -1,7 +1,7 @@
 export const BASE_URL =
-  'https://finance-club-backend.studentvlad5.repl.co/api';
+  'https://finance-clubbackend.studentvlad5.repl.co/api';
 export const BASE_URL_IMG =
-  'https://finance-club-backend.studentvlad5.repl.co/uploads/';
+  'https://finance-clubbackend.studentvlad5.repl.co/uploads/';
 
 // export const BASE_URL = 'http://localhost:3030/api';
 // export const BASE_URL_IMG = 'http://localhost:3030/uploads/';
